6a19d98f1740062941f08d3c69b383c7acf18f0b
sebi <sebisebpaw@gmail.com>
2026-04-26 01:12:51 -0500

    remove unessecary il2cpp registration

--- InventoryItem.cs
+++ InventoryItem.cs
@@ -2,18 +2,15 @@
 
 namespace InventoryFramework
 {
-    [RegisterTypeInIl2Cpp]
-    public class InventoryItem : Il2CppSystem.Object
+    public class InventoryItem
     {
         public ItemDefinition Definition { get; init; }
         public int Quantity { get; set; }
-        
+
         public InventoryItem(ItemDefinition definition, int quantity)
         {
             Definition = definition;
             Quantity = quantity;
         }
-
-        public InventoryItem(IntPtr ptr) : base(ptr) { }
     }
 }
